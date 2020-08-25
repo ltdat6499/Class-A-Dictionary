867ec1ddc18606e3ffe0a2ee82a2ce87404456f8
Lý Thành Đạt <47844380+ltdat6499@users.noreply.github.com>
2020-08-26 00:15:46 +0700

    My First Dev Project 2016

---
+++ CongthuclythuyetthiTHPT2016khoiA/app/src/test/java/congthuckhoia/ly_thanh_dat/thpt_nguyen_van_troi/congthuclythuyetthithpt2016khoia/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package congthuckhoia.ly_thanh_dat.thpt_nguyen_van_troi.congthuclythuyetthithpt2016khoia;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
